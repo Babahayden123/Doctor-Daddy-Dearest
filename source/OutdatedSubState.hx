a99b369357ab64cf46b15ffa89210f580bd06f28
Collin Zhang <87448560+whitty20012@users.noreply.github.com>
2021-09-20 13:37:51 -0700

    changed my mind

--- source/OutdatedSubState.hx
+++ source/OutdatedSubState.hx
@@ -22,7 +22,7 @@ class OutdatedSubState extends MusicBeatState
 			+ ver
 			+ " while the most recent version is "
 			+ NGio.GAME_VER
-			+ "! Press Space to go to itch.io, or ESCAPE to ignore this!!",
+			+ "! Press Space to go to itch.io, or ESCAPE to ignore this!!", // idk what to say
 			32);
 		txt.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		txt.screenCenter();
