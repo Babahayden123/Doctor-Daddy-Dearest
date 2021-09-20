0b94f3d91e346738629a53a878fff062c38f80cd
Collin Zhang <87448560+whitty20012@users.noreply.github.com>
2021-09-20 09:03:20 -0700

    Fixes

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -188,7 +188,7 @@ class TitleState extends MusicBeatState
 		blackScreen = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
 		credGroup.add(blackScreen);
 
-		credTextShit = new Alphabet(0, 0, "ninjamuffin99\nPhantomArcade\nkawaisprite\nevilsk8er", true);
+		credTextShit = new Alphabet(0, 0, "ninjamuffin99\nPhantomArcade\nkawaisprite\nevilsk8er\nwhitty20012", true);
 		credTextShit.screenCenter();
 
 		// credTextShit.alignment = CENTER;
